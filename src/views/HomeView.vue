d6b1f105e28be8844509173774265888ef03d4a6
hungVietTa <viethunglpt@gmail.com>
2022-09-07 22:35:50 +0700

    kl

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,37 +2,56 @@
   <div class="home">
     <div class="feature-card">
       <router-link to="/movie/tt0409591">
-        <img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51yPFJAL9iL._AC_.jpg" class="feature-img" alt="Kiki's delivery service">
+        <img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51yPFJAL9iL._AC_.jpg" class="feature-img"
+          alt="Kiki's delivery service">
       </router-link>
       <div class="detail">
         <h3>Kiki's delivery service</h3>
-        <p>Thirteen-year-old trainee witch Kiki leaves home with her talking black cat Jiji. She flies on her broomstick to the port city of Koriko.</p>
+        <p>Thirteen-year-old trainee witch Kiki leaves home with her talking black cat Jiji. She flies on her broomstick
+          to the port city of Koriko.</p>
       </div>
     </div>
-     <form @submit.prevent="SearchMovie()" class="search-box">
-    <input type="text" placeholder="What are you looking for ?">
-    <input type="submit" value="Search">
-  </form>
-  <div class="movie-list">
-    
-  </div>
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="What are you looking for ?" v-model="search">
+      <input type="submit" value="Search">
+    </form>
+    <div class="movie-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/'+movie.imdbID" class="movie-link">
+        <div class="product-image">
+          <img :src="movie.Poster" alt="Movie Poster" />
+          <div class="type">{{movie.Type}}</div>
+        </div>
+        <div class="detail">
+          <p class="year">{{movie.Year}}</p>
+          <h3>{{movie.Title}}</h3>
+        </div>
+      </router-link>
+      </div>
+    </div>
   </div>
 
- 
+
 </template>
 
 <script>
-// @ is an alias to /src
-import {ref} from 'vue';
+import env from '@/env.js'
+import { ref } from 'vue';
 
 export default {
   setup() {
     const search = ref("");
-    const movie = ref([]);
+    const movies = ref([]);
 
     const SearchMovies = () => {
       if (search.value != "") {
-        console.log(search.value)
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+            console.log(data)
+          })
       }
     }
     return {
@@ -47,6 +66,7 @@ export default {
 .home {
   .feature-card {
     position: relative;
+
     .feature-img {
       display: block;
       width: 100%;
@@ -55,12 +75,13 @@ export default {
       position: relative;
       z-index: 0;
     }
+
     .detail {
       position: absolute;
       left: 0;
       right: 0;
       bottom: 0;
-      background-color: rgba(0,0,0,0.6);
+      background-color: rgba(0, 0, 0, 0.6);
       padding: 16px;
       z-index: 1;
 
@@ -68,22 +89,24 @@ export default {
         color: #fff;
         margin-bottom: 16px;
       }
+
       p {
         color: white;
       }
     }
   }
+
   .search-box {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding:16px;
+    padding: 16px;
 
     input {
       display: block;
       appearance: none;
-      border:none;
+      border: none;
       outline: none;
       background: none;
 
@@ -98,12 +121,14 @@ export default {
         transition: 0.4s;
 
         &::placeholder {
-        color: #f3f3f3;
+          color: #f3f3f3;
         }
+
         &:focus {
-        box-shadow: 0px 3px 6px rgba(0,0,0,0.2);
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
         }
       }
+
       &[type="submit"] {
         width: 100%;
         max-width: 300px;
@@ -114,12 +139,71 @@ export default {
         font-size: 20px;
         text-transform: uppercase;
         transition: 0.4s;
+        cursor: pointer;
 
         &:active {
           background-color: #3b8070;
         }
       }
+    }
   }
+
+  .movie-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+            
+
+            .type {
+              position: absolute;
+              padding: 8px 16px;
+              background-color: #42b883;
+              color: white;
+              bottom: 16px;
+              left: 0px;
+              text-transform: capitalize;
+            }
+          }
+          .detail {
+            background-color: #496583;
+            padding: 16px 8px;
+            flex: 1 1 100%;
+            border-radius: 0 0 8px 8px;
+
+            .year {
+              color: #AAA;
+              font-size: 14px;
+            }
+
+            h3 {
+              color: #FFF;
+              font-weight: 600;
+              font-size: 18px;
+            }
+          }
+        }
+      }
+    }
   }
-}
 </style>
